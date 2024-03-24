6dabc4ccaf23d1e1d12669ee23a126ae38950aca
HalaBarbary <hala.elbarbary@gmail.com>
2024-03-24 15:03:55 -0700

    print last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number != 0:
-        print("{:d}".format(abs(number) % 10))
-    else:
-        print(number)
-    return (number % 10)
+    l_dig = abs(number % 10)
+    print("{:d}".format(l_dig))
+    return (l_dig)
