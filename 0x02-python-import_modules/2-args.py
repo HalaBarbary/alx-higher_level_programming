95bc51bf6adaac5f00d74c66841b1086f32210ce
HalaBarbary <hala.elbarbary@gmail.com>
2024-04-12 18:10:57 -0700

    return number of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     elif len(sys.argv) > 2:
-        print("{:d} arguments".format(len(sys.argv)-1))
+        print("{:d} arguments:".format(len(sys.argv)-1))
         for i in range(1, len(sys.argv)):
             print("{:d}: {} ".format(i, sys.argv[i]))
