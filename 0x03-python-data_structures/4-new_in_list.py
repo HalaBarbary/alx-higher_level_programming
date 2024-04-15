e4eed0aa4914cb6152fee3444367dc8650949a7d
HalaBarbary <hala.elbarbary@gmail.com>
2024-04-15 15:45:34 -0700

    replace an item at a certain index

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     if (len(my_list)-1) < idx < 0:
         list = my_list
     else:
-        my_list[idx] = element
         list = my_list
+        list[idx] = element
     return(list)
