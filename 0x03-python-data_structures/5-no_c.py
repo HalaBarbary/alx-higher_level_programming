f49c0fb4d13455ffb52caa5d1a79740060fd1d01
HalaBarbary <hala.elbarbary@gmail.com>
2024-04-16 15:38:35 -0700

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    no_c = "cC"
     for letter in my_string:
-        if letter == "c":
-            my_string[letter] = ""
-        elif letter == "C":
-            my_string[letter] = ""
-    return (my_string)
+        if letter not in no_c:
+            modified_str += letter
+    return (modified_str)
