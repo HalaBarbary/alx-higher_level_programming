97c35c49768735c027c9a81ede23b8a77a2cdce4
HalaBarbary <hala.elbarbary@gmail.com>
2024-04-16 15:28:35 -0700

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     for letter in my_string:
         if letter == "c":
-            my_string[letter].replace("c", "")
+            my_string[letter] = ""
         elif letter == "C":
-            my_string[letter].replace("C", "")
+            my_string[letter] = ""
     return (my_string)
