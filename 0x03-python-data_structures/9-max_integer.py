164ff0215fc5e0685aff9f321e0b7756f93c4976
HalaBarbary <hala.elbarbary@gmail.com>
2024-04-23 15:46:09 -0700

    return max integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,5 +3,5 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        sorted_lst = list.sort(my_list)
+        sorted_lst = sorted(my_list)
         return (sorted_lst[-1])
