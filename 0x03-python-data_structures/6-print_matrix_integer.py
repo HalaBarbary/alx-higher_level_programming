d8e11de2f83e6fa44c6ab76cb9b0382eab9a9e1b
HalaBarbary <hala.elbarbary@gmail.com>
2024-04-23 13:42:39 -0700

    print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,5 @@
+#!/bin/usr/python3
+def print_matrix_integer(matrix=[[]]):
+    for list in (len(matrix)-1):
+        for item in matrix[list]:
+            print("{}".format(matrix[list[item]]), end=" ")
