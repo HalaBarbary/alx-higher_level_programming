5a1cbb2a453a43023053e89252a37275492f25ff
HalaBarbary <hala.elbarbary@gmail.com>
2024-04-23 14:41:51 -0700

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for lst in matrix:
-        if lst < len(matrix):
+        if lst != matrix[-1]:
             for item in lst:
-                if item < len(lst):
+                if item != lst[-1]:
                     print("{:d}".format(matrix[lst[item]]), end=" ")
