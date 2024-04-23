dded429c7cc83e61370e14434eeaa1807641a492
HalaBarbary <hala.elbarbary@gmail.com>
2024-04-23 14:43:37 -0700

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,4 @@ def print_matrix_integer(matrix=[[]]):
         if lst != matrix[-1]:
             for item in lst:
                 if item != lst[-1]:
-                    print("{:d}".format(matrix[lst[item]]), end=" ")
+                    print("{:d}".format(item), end=" ")
